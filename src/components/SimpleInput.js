8da476aa3472a1be75834f8d3a78215dbff96a75
juliodiaz <julio@vimbly.com>
2021-06-08 22:53:31 -0500

    disable submit button

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,33 +1,23 @@
-import {useRef, useState, useEffect} from 'react';
+import {useState} from 'react';
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  useEffect(() => {
-    if(enteredNameIsValid){
-      console.log('>>>')
-    }
-  }, [enteredNameIsValid])
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  let formIsValid = false;
+
+  if(enteredNameIsValid){
+    formIsValid = true;
+  }
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value); // The entered value is not updated inmediatly
-
-    if(event.target.value.trim() !== ''){
-      setEnteredNameIsValid(true);
-      return;
-    }
   }
 
   const nameInputBlurHandler = event => {
     setEnteredNameTouched(true);
-
-    if(enteredName.trim() === ''){
-      setEnteredNameIsValid(false);
-      return;
-    }
   }
 
   const formSubmissionHandler = event => {
@@ -35,22 +25,13 @@ const SimpleInput = (props) => {
 
     setEnteredNameTouched(true);
 
-    if(enteredName.trim() === ''){
-      setEnteredNameIsValid(false);
+    if(!enteredNameIsValid){
       return;
     }
 
-    console.log(enteredName);
-
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue);
-
-    // nameInputRef.current.value = ''; // NOT IDEAL, DON'T MANUPULATE THE DOM
     setEnteredName('');
-    setEnteredNameIsValid(true);
+    setEnteredNameTouched(false);
   }
-
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
 
@@ -59,7 +40,6 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input 
-          ref={nameInputRef} 
           onBlur={nameInputBlurHandler} // Focus
           type='text' 
           value={enteredName} 
@@ -68,7 +48,7 @@ const SimpleInput = (props) => {
         {nameInputIsInvalid && <p className='error-text'>Name must be empty</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
